06471757bac6956d8227a3bf1ebd7c9fd59f447f
gexayr <mgexayr@smartbet.am>
2022-09-16 18:29:18 +0400

    crud express

---
+++ index.js
@@ -0,0 +1,74 @@
+import express from "express";
+
+const app = express();
+
+let events = {};
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+// CREATE
+app.post("/events", (req, res) => {
+    const id = `event_${Date.now()}_${Math.random()}`;
+    const newEvent = {
+      id,
+      ...req.body
+    };
+
+    events[id] = newEvent;
+    res.send(newEvent);
+});
+
+//READ
+app.get("/events/:id", (req, res) => {
+    const event = events[req.params.id];
+    if(event === undefined) {
+        res.status(404).send('Event not found');
+    } else {
+        res.send(event);
+    }
+});
+
+// UPDATE
+app.get("/events/:id", (req, res) => {
+    const event = events[req.params.id];
+    if (event === undefined) {
+        res.status(404).send("Event not found");
+    } else {
+        delete events[req.params.id];
+        res.send();
+    }
+});
+
+// DELETE
+app.delete("/events/:id", (req, res) => {
+    const event = events[req.params.id];
+    if(event === undefined) {
+        res.status(404).send('Event not found');
+    } else {
+        delete events[req.params.id];
+        res.send();
+    }
+});
+
+// SEARCH
+app.get("/events", (req, res) => {
+    const {type, q} = req.query;
+    const results = Object.values(events).filter(event => {
+        if(type !== undefined && type !== event.type) {
+            return false;
+        }
+        if (q !== undefined && event.name.search(new RegExp(q, 'i')) === -1) {
+            return false;
+        }
+        return true;
+    });
+    res.send(results);
+});
+
+
+
+app.listen(process.env.PORT || 5000);
